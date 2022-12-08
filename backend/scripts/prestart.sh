d5b2c569ce20257c5a15ac9e4942f4291d0458fc
Joey Grable <hello@joeygrable.com>
2022-12-07 19:58:24 -0800

    removed ununsed backend services: redis, whoami

--- backend/scripts/prestart.sh
+++ backend/scripts/prestart.sh
@@ -4,9 +4,9 @@ set -e
 echo "Prestarting Backend API..."
 
 # Check the DB is connected.
-python /app/app/prestart.py
+python /app/prestart.py
 sleep 1;
 
 # Run DB migrations
-alembic upgrade head
-sleep 1;
+# alembic upgrade head
+# sleep 1;
