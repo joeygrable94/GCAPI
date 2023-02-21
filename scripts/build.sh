5aef5032a00a9f22b06eb29890084712f21853d0
Joey Grable <hello@joeygrable.com>
2023-02-20 18:31:13 -0800

    improved the docker compose for swarm mode

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,13 @@
 # Exit in case of error
 set -e
 
+# Import .env vars
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
 TAG=${TAG?Variable not set} \
 VITE_APP_ENV=${VITE_APP_ENV-production} \
-docker-compose \
+docker compose \
 -f docker-compose.yml \
 build
