5fd4cfe5434efe7db1005cc8337c37eeca567bbe
Joey Grable <hello@joeygrable.com>
2023-04-13 16:11:05 -0700

    updated testing script and backend commit ref

--- scripts/test.sh
+++ scripts/test.sh
@@ -19,5 +19,5 @@ config > docker-stack.yml
 docker compose -f docker-stack.yml build
 docker compose -f docker-stack.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
 docker compose -f docker-stack.yml up -d
-docker compose -f docker-stack.yml exec -T backend bash /start-tests.sh "$@"
+docker compose -f docker-stack.yml exec -T backend bash /start-tests.sh
 docker compose -f docker-stack.yml down -v --remove-orphans
