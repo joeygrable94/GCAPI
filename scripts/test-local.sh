7ca0b7677a945f7a503de162a0948e973ea6b369
Joey Grable <hello@joeygrable.com>
2023-04-13 16:38:19 -0700

    updated test-local.sh

--- scripts/test-local.sh
+++ scripts/test-local.sh
@@ -8,13 +8,13 @@ if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
-# docker compose down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
+docker compose down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
 
 if [ $(uname -s) = "Linux" ]; then
     echo "Remove __pycache__ files"
     sudo find . -type d -name __pycache__ -exec rm -r {} \+
 fi
 
-# docker compose build
+docker compose build
 docker compose up -d
 docker compose exec -T backend bash ./scripts/test.sh
