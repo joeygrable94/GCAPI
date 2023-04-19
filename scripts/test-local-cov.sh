3e2380cbb394f43e0b4559429374dbb8450fc216
Joey Grable <hello@joeygrable.com>
2023-04-19 08:42:18 -0700

    updated backend test scripts

--- scripts/test-local-cov.sh
+++ scripts/test-local-cov.sh
@@ -8,11 +8,15 @@ if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
+# Remove possibly previous broken stacks left hanging after an error
+docker compose down -v --remove-orphans
+
 # Remove pycache files
 if [ $(uname -s) = "Linux" ]; then
     echo "Remove __pycache__ files"
     sudo find . -type d -name __pycache__ -exec rm -r {} \+
 fi
 
+docker compose build
 docker compose up -d
 docker compose exec -T backend bash ./scripts/test-cov.sh
